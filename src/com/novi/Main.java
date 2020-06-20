109896b58bb599931c3625fa661e162fae700caa
misterbennie <ben.de.vette@profiler.nl>
2020-06-20 22:47:49 +0200

    Commit of 3 extra files Person, School, Student

--- src/com/novi/Main.java
+++ src/com/novi/Main.java
@@ -3,6 +3,6 @@ package com.novi;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
---
+++ src/com/novi/Student.java
@@ -0,0 +1,4 @@
+package com.novi;
+
+public class Student {
+}
